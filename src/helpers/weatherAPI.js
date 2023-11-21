7f48073ceef4e141c678e3d43a13422934d61c48
Guilherme Alvarez <guilherme.zalv@gmail.com>
2023-11-21 11:24:13 -0300

    Primeiro Req

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,9 +1,43 @@
 // Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  const resultado = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
+  console.log(resultado);
+  const data = await resultado.json();
+
+  if (data.length === 0) {
+    console.log('Nenhuma cidade encontrada');
+    return alert('Nenhuma cidade encontrada');
+  }
+  console.log(data);
+  // console.log(data[0].url);
+  // const resultadoTempo = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${data[0].url}`);
+  // const dataTempo = await resultadoTempo.json();
+  // console.log(dataTempo);
+  // console.log(`temp: ${dataTempo.current.temp_c}`);
+  // return {
+  //   "temp": dataTempo.current.temp_c,
+  //   "condition": dataTempo.current.condition,
+  //   "icon": dataTempo.current.condition.icon,
+  //   "country": dataTempo.location.country,
+  //   "name": dataTempo.location.name,
+  //   "url": cityURL
+  // }
+
+  return data;
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
+export const getWeatherByCity = async (cityURL) => {
+  const resultadoTempo = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${cityURL}`);
+  const dataTempo = await resultadoTempo.json();
+  console.log(dataTempo);
+  return {
+    temp: dataTempo.current.temp_c,
+    condition: dataTempo.current.condition,
+    icon: dataTempo.current.condition.icon,
+    country: dataTempo.location.country,
+    name: dataTempo.location.name,
+    url: cityURL,
+  };
 };
